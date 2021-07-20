651e91e82a0bdd993805db2000d5f1059f717a26
zhaoyang <y.zhao@avid.ly>
2021-07-20 11:11:35 +0800

    新增Campaign Create和Find

---
+++ src/Technology/SearchAds/CampaignCreate.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Snow\Apple\Technology\SearchAds;
+
+
+class CampaignCreate extends SearchAds
+{
+
+    private $url = 'https://api.searchads.apple.com/api/v4/campaigns';
+
+    /**
+     * Notes: Creates a campaign to promote an app
+     * @param array $campaign
+     * @param mixed ...$params
+     * @return mixed|string
+     * @throws SearchAdsException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function execute($campaign, ...$params)
+    {
+        return $this->parseResponse($this->quickClient()->post($this->url, ['json' => $campaign]));
+    }
+}
